2159582318135e844adfd57c8712e5cd440c59d7
Daniel Grace <negativeview@gmail.com>
2019-03-31 14:17:51 -0500

    Move the parameter to the entire class. Ugh.

--- src/main/java/Marketplace.java
+++ src/main/java/Marketplace.java
@@ -3,18 +3,18 @@ package com.gracefulcode.opengine;
 import com.github.opengine.resource.Resource;
 import java.util.HashMap;
 
-public class Marketplace {
-	protected HashMap<Class<? extends Resource>, Integer> supply;
-	protected HashMap<Class<? extends Resource>, Integer> demand;
+public class Marketplace<T extends Class<? extends Resource>> {
+	protected HashMap<T, Integer> supply;
+	protected HashMap<T, Integer> demand;
 
-	public <T extends Class<? extends Resource>> void addSupply(T cls, int amount) {
+	public void addSupply(T cls, int amount) {
 		if (this.supply == null) {
 			this.supply = new HashMap<T, Integer>();
 		}
 		this.supply.put(cls, this.supply.get(cls) + amount);
 	}
 
-	public <T extends Class<? extends Resource>> void addDemand(T cls, int amount) {
+	public void addDemand(T cls, int amount) {
 		if (this.demand == null) {
 			this.demand = new HashMap<T, Integer>();
 		}
