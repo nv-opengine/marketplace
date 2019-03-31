15cab18fcc7d5aceb9d0efd1d223f4b5fc7a7e8a
Daniel Grace <negativeview@gmail.com>
2019-03-30 20:42:56 -0500

    Stop being static and add a toString.

--- src/main/java/Marketplace.java
+++ src/main/java/Marketplace.java
@@ -1,5 +1,24 @@
 package com.gracefulcode.opengine;
 
 public class Marketplace {
-	
+	protected HashMap<Class<? extends Resource>, Integer> supply;
+	protected HashMap<Class<? extends Resource>, Integer> demand;
+
+	public void addSupply(Class<? extends Resource> cls, int amount) {
+		if (Marketplace.supply == null) {
+			Marketplace.supply = new HashMap<cls, Integer>();
+		}
+		Marketplace.supply.put(cls, Marketplace.supply.get(cls) + amount);
+	}
+
+	public void addDemand(Class<? extends Resource> cls, int amount) {
+		if (Marketplace.demand == null) {
+			Marketplace.demand = new HashMap<cls, Integer>();
+		}
+		Marketplace.supply.put(cls, Marketplace.demand.get(cls) + amount);
+	}
+
+	public String toString() {
+		return "Market: supply: " + this.supply.toString() + ", demand: " + this.demand.toString();
+	}
 }
