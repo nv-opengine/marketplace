60048a03d3c36cf440cf73bbddae4c874faf2e70
Daniel Grace <negativeview@gmail.com>
2019-03-31 14:22:52 -0500

    Have a default value instead of throwing an exception.

--- src/main/java/Marketplace.java
+++ src/main/java/Marketplace.java
@@ -11,14 +11,14 @@ public class Marketplace<T extends Class<? extends Resource>> {
 		if (this.supply == null) {
 			this.supply = new HashMap<T, Integer>();
 		}
-		this.supply.put(cls, this.supply.get(cls) + amount);
+		this.supply.put(cls, this.supply.get(cls, 0) + amount);
 	}
 
 	public void addDemand(T cls, int amount) {
 		if (this.demand == null) {
 			this.demand = new HashMap<T, Integer>();
 		}
-		this.supply.put(cls, this.demand.get(cls) + amount);
+		this.supply.put(cls, this.demand.get(cls, 0) + amount);
 	}
 
 	public String toString() {
