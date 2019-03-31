c9f1a46b803a357a6aa559eac8807d13ae81ae62
Daniel Grace <negativeview@gmail.com>
2019-03-31 14:08:29 -0500

    Stop being static.

--- src/main/java/Marketplace.java
+++ src/main/java/Marketplace.java
@@ -8,17 +8,17 @@ public class Marketplace {
 	protected HashMap<Class<? extends Resource>, Integer> demand;
 
 	public void addSupply(Class<? extends Resource> cls, int amount) {
-		if (Marketplace.supply == null) {
-			Marketplace.supply = new HashMap<cls, Integer>();
+		if (this.supply == null) {
+			this.supply = new HashMap<cls, Integer>();
 		}
-		Marketplace.supply.put(cls, Marketplace.supply.get(cls) + amount);
+		this.supply.put(cls, this.supply.get(cls) + amount);
 	}
 
 	public void addDemand(Class<? extends Resource> cls, int amount) {
-		if (Marketplace.demand == null) {
-			Marketplace.demand = new HashMap<cls, Integer>();
+		if (this.demand == null) {
+			this.demand = new HashMap<cls, Integer>();
 		}
-		Marketplace.supply.put(cls, Marketplace.demand.get(cls) + amount);
+		this.supply.put(cls, this.demand.get(cls) + amount);
 	}
 
 	public String toString() {
