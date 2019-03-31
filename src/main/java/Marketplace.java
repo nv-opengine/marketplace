698bfc300f661ca7c7dbe2d33171916ad386217f
Daniel Grace <negativeview@gmail.com>
2019-03-31 14:15:22 -0500

    I am bad at syntax.

--- src/main/java/Marketplace.java
+++ src/main/java/Marketplace.java
@@ -7,14 +7,14 @@ public class Marketplace {
 	protected HashMap<Class<? extends Resource>, Integer> supply;
 	protected HashMap<Class<? extends Resource>, Integer> demand;
 
-	public void <T extends Class<? extends Resource>> addSupply(T cls, int amount) {
+	public <T extends Class<? extends Resource>> void addSupply(T cls, int amount) {
 		if (this.supply == null) {
 			this.supply = new HashMap<T, Integer>();
 		}
 		this.supply.put(cls, this.supply.get(cls) + amount);
 	}
 
-	public void <T extends Class<? extends Resource>> addDemand(T cls, int amount) {
+	public <T extends Class<? extends Resource>> void addDemand(T cls, int amount) {
 		if (this.demand == null) {
 			this.demand = new HashMap<T, Integer>();
 		}
